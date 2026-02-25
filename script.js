03dafcdd17edb038583e346b96b3ff39b935ba40
hemantjain21 <109237389+hemantjain21@users.noreply.github.com>
2026-02-25 10:33:44 +0530

    Create function returning human choice

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let randomChoice = Math.floor(Math.random()*100)+1;
     if(randomChoice <= 33){
@@ -9,4 +12,8 @@ function getComputerChoice(){
     }
 }
 
-
+function getHumanChoice(){
+    let humanChoice = prompt("Enter your choice: rock / paper / scissors?");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
+}
