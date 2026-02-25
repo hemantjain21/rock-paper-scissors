16fe7fbe800402fcae8745fa7f19a3825d443cde
hemantjain21 <109237389+hemantjain21@users.noreply.github.com>
2026-02-25 11:08:19 +0530

    Create playRound func with both choice func as arguments

--- script.js
+++ script.js
@@ -18,3 +18,41 @@ function getHumanChoice(){
     return playerChoice;
 }
 
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === "rock"){
+        if(computerChoice === "paper"){
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        } else if(computerChoice === "scissors"){
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        } else {
+            console.log(`Draw! Both you and computer chose ${humanChoice}`);
+        }
+
+    } else if(humanChoice === "paper"){
+        if(computerChoice === "rock"){
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        } else if(computerChoice === "scissors"){
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        } else {
+            console.log(`Draw! Both you and computer chose ${humanChoice}`);
+        }
+
+    } else {
+        if(computerChoice === "rock"){
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        } else if(computerChoice === "paper"){
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        } else {
+            console.log(`Draw! Both you and computer chose ${humanChoice}`);
+        }
+    }
+    
+}
+
+playRound(getHumanChoice(), getComputerChoice());
